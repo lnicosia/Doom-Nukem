df7112dea86b8581b19090f32688abdbb0a5e41b
lnicosia <lnicosia@student.42.fr>
2019-04-23 10:38:31 +0200

    Modified starting options.

--- src/init_options.c
+++ src/init_options.c
@@ -6,7 +6,7 @@
 /*   By: gaerhard <gaerhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 15:16:14 by lnicosia          #+#    #+#             */
-/*   Updated: 2019/04/22 11:03:01 by lnicosia         ###   ########.fr       */
+/*   Updated: 2019/04/23 10:37:36 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	init_options(t_env *env)
 	env->options.render_sectors = 1;
 	env->options.lighting = 0;
 	env->options.show_minimap = 1;
-	env->options.wall_lover = 1;
+	env->options.wall_lover = 0;
 	env->options.show_fps = 1;
 	env->options.color_clipping = 0;
 }
