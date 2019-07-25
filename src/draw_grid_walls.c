cd0ac11639e9d82c31d5d7e8800b95cb5063c3e1
Simon PATRY <sipatry@e1r10p10.42.fr>
2019-07-25 15:19:25 +0200

    editor without print

--- src/draw_grid_walls.c
+++ src/draw_grid_walls.c
@@ -6,7 +6,7 @@
 /*   By: sipatry <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/25 13:22:35 by sipatry           #+#    #+#             */
-/*   Updated: 2019/07/25 14:58:04 by sipatry          ###   ########.fr       */
+/*   Updated: 2019/07/25 15:19:02 by sipatry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,8 @@ void	draw_grid_walls(t_env *env)
 	while (tmp)
 	{
 		v = (t_vertex*)tmp->content;
-		print_vertex(env, v->num);
 		v2.x = env->edit.center.x + v->x * env->edit.scale;
 		v2.y = env->edit.center.y + v->y * env->edit.scale;
-		ft_printf("v1 = [%d][%d] v2 = [%d][%d]\n", v1.y, v1.x, v2.y, v2.x);
 		draw_line(v1, v2, *env, 0xFFFFFF00);
 		v1 = v2;
 		tmp = tmp->next;
