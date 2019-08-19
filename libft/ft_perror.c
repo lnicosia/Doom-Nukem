3b04de75cdb5f3d868e7b9c04ffee1b4caff44d3
lnicosia <lnicosia@student.42.fr>
2019-08-19 18:46:13 +0200

    Parser protection messages rework in progress. Player, vertices done. Sectors in progress. Added ft_perror to libft: calls perro with the given string and returns -1.

--- libft/ft_perror.c
+++ libft/ft_perror.c
@@ -6,12 +6,16 @@
 /*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 16:37:09 by lnicosia          #+#    #+#             */
-/*   Updated: 2019/08/19 16:38:34 by lnicosia         ###   ########.fr       */
+/*   Updated: 2019/08/19 18:46:05 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
+
+/*
+**	Calls perror with the given string then returns -1
+*/
 
 int		ft_perror(const char *s)
 {
