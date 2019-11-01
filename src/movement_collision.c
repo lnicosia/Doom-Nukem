d43873fe57dda6d4367ae945e86b11690c7b44ab
Gabriel ERHARD <gaerhard@student.42.fr>
2019-11-01 17:28:09 +0100

    added a vertical collision even when staying in the same sector for ceiling slopes

--- src/movement_collision.c
+++ src/movement_collision.c
@@ -6,7 +6,7 @@
 /*   By: gaerhard <gaerhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 17:45:07 by gaerhard          #+#    #+#             */
-/*   Updated: 2019/10/23 16:05:55 by gaerhard         ###   ########.fr       */
+/*   Updated: 2019/11/01 17:27:12 by gaerhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -148,7 +148,9 @@ t_v2     check_collision(t_env *env, t_v2 move, t_movement motion, int rec)
     i = 0;
     init_sector_list(env, motion.sector);
     if (motion.sector == -1)
-        return (new_v2(0,0));
+        return (new_v2(0, 0));
+	if (!check_ceiling(env, motion, motion.sector))
+		return (new_v2(0, 0));
     while (i < env->sectors[motion.sector].nb_vertices)
     {
         if ((hitbox_collision(new_v2(X1, Y1), new_v2(X2, Y2), new_v2(FUTURE_X, FUTURE_Y), motion.size_2d)) && NEIGHBOR < 0)
