858c33fd49d8f1c7c134fb0243e1ec851ca1302e
lnicosia <lnicosia@student.42.fr>
2020-01-06 17:11:50 +0100

    [ADD] Press event wall sprites now have a minimum press distance.

--- src/draw_wall_sprites.c
+++ src/draw_wall_sprites.c
@@ -6,7 +6,7 @@
 /*   By: sipatry <sipatry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 18:48:09 by lnicosia          #+#    #+#             */
-/*   Updated: 2019/12/04 15:13:45 by lnicosia         ###   ########.fr       */
+/*   Updated: 2020/01/06 17:10:36 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "render.h"
 
 void	draw_vline_sprite(int sprite, t_sector sector, t_render render,
-t_env *env)
+		t_env *env)
 {
 	int	i;
 	int	coord;
@@ -34,9 +34,9 @@ t_env *env)
 	zbuffer = env->zbuffer;
 	pixels = env->sdl.texture_pixels;
 	sprite_pixels = env->sprite_textures[env->wall_sprites[sector.wall_sprites[render.i]
-	.sprite[sprite]].texture].str;
+		.sprite[sprite]].texture].str;
 	sprite_w = env->sprite_textures[env->wall_sprites[sector.wall_sprites[render.i]
-	.sprite[sprite]].texture].surface->w;
+		.sprite[sprite]].texture].surface->w;
 	pos = sector.wall_sprites[render.i].pos[sprite].y / (sector.ceiling - sector.floor);
 	start = env->wall_sprites[sector.wall_sprites[render.i].sprite[sprite]].start[0].y;
 	end = env->wall_sprites[sector.wall_sprites[render.i].sprite[sprite]].end[0].y;
@@ -47,17 +47,17 @@ t_env *env)
 		if (render.z >= zbuffer[coord])
 			continue;
 		yalpha = (i - render.no_slope_current_ceiling)
-		/ render.line_height;
+			/ render.line_height;
 		/*x = yalpha * render.camera->v[render.sector]
-		[render.i].sprite_scale[sprite].y + start;
-		if (x >= start && x < end
-			&& sprite_pixels[(int)x
-			+ sprite_w * (int)y] != 0xFFC10099)*/
+		  [render.i].sprite_scale[sprite].y + start;
+		  if (x >= start && x < end
+		  && sprite_pixels[(int)x
+		  + sprite_w * (int)y] != 0xFFC10099)*/
 		y = (yalpha - pos) * render.camera->v[render.sector]
-		[render.i].sprite_scale[sprite].y + start;
+			[render.i].sprite_scale[sprite].y + start;
 		if (y >= start && y < end
-			&& sprite_pixels[(int)x
-			+ sprite_w * (int)y] != 0xFFC10099)
+				&& sprite_pixels[(int)x
+				+ sprite_w * (int)y] != 0xFFC10099)
 		{
 			if (render.x == env->h_w && i == env->h_h)
 			{
@@ -69,39 +69,48 @@ t_env *env)
 					env->editor.selected_sector = sector.num;
 				}
 				if (env->playing
-					&& sector.wall_sprites[render.i].nb_press_events[sprite])
+						&& sector.wall_sprites[render.i].nb_press_events[sprite])
 				{
-					env->hovered_wall_sprite_wall = render.i;
-					env->hovered_wall_sprite_sprite = sprite;
-					env->hovered_wall_sprite_sector = sector.num;
+					if (render.z < 10)
+					{
+						env->hovered_wall_sprite_wall = render.i;
+						env->hovered_wall_sprite_sprite = sprite;
+						env->hovered_wall_sprite_sector = sector.num;
+					}
+					else
+					{
+						env->hovered_wall_sprite_wall = -1;
+						env->hovered_wall_sprite_sprite = -1;
+						env->hovered_wall_sprite_sector = -1;
+					}
 				}
 			}
 			if (!env->options.lighting
-				|| (!sector.brightness && !sector.intensity))
+					|| (!sector.brightness && !sector.intensity))
 				pixels[coord] = sprite_pixels[
-				(int)x + sprite_w * (int)y];
+					(int)x + sprite_w * (int)y];
 			else if (!sector.brightness)
 				pixels[coord] = apply_light_color(sprite_pixels[
-				(int)x + sprite_w * (int)y],
-				sector.light_color, sector.intensity);
+						(int)x + sprite_w * (int)y],
+						sector.light_color, sector.intensity);
 			else if (!sector.intensity)
 				pixels[coord] = apply_light_brightness(sprite_pixels[
-				(int)x + sprite_w * (int)y],
-				sector.brightness);
+						(int)x + sprite_w * (int)y],
+						sector.brightness);
 			else
 				pixels[coord] = apply_light_both(sprite_pixels[
-				(int)x + sprite_w * (int)y],
-				sector.light_color, sector.intensity, sector.brightness);
+						(int)x + sprite_w * (int)y],
+						sector.light_color, sector.intensity, sector.brightness);
 			if (!env->editor.select
-				&& env->editor.selected_sector == sector.num
-				&& env->selected_wall_sprite_wall == render.i
-				&& env->selected_wall_sprite_sprite == sprite)
+					&& env->editor.selected_sector == sector.num
+					&& env->selected_wall_sprite_wall == render.i
+					&& env->selected_wall_sprite_sprite == sprite)
 				pixels[coord] = blend_alpha(pixels[coord], 0x1ABC9C, 128);
 			else if (env->playing && env->hovered_wall_sprite_wall == render.i
-				&& env->hovered_wall_sprite_sprite == sprite)
+					&& env->hovered_wall_sprite_sprite == sprite)
 				pixels[coord] = blend_alpha(pixels[coord],
-				env->press_wall_sprite_color,
-				env->press_wall_sprite_intensity);
+						env->press_wall_sprite_color,
+						env->press_wall_sprite_intensity);
 			zbuffer[coord] = render.z;
 		}
 	}
@@ -122,18 +131,18 @@ void	draw_wall_sprites(t_sector sector, t_render render, t_env *env)
 			start = env->wall_sprites[sector.wall_sprites[render.i].sprite[i]].start[0];
 			end = env->wall_sprites[sector.wall_sprites[render.i].sprite[i]].end[0];
 			pos = (sector.wall_sprites[render.i].pos[i].x)
-			/ sector.wall_width[render.i]
-			* render.camera->v[render.sector][render.i].sprite_scale[i].x;
+				/ sector.wall_width[render.i]
+				* render.camera->v[render.sector][render.i].sprite_scale[i].x;
 			if (render.camera->v[render.sector][render.i + 1].vz)
 				pos *= render.camera->v[render.sector][render.i + 1].vz;
 			else
 				pos *= render.camera->v[render.sector][render.i].clipped_vz2;
 			render.sprite_x = (render.alpha)
-			* render.camera->v[render.sector][render.i].sprite_scale[i].x
-			* render.z + start.x
-			- pos;
+				* render.camera->v[render.sector][render.i].sprite_scale[i].x
+				* render.z + start.x
+				- pos;
 			if (render.sprite_x >= start.x
-				&& render.sprite_x < end.x)
+					&& render.sprite_x < end.x)
 				draw_vline_sprite(i, sector, render, env);
 		}
 		i++;
