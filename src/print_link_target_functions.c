4c6c3d06b7106082226621e88a0d426f3101fcc4
lnicosia <lnicosia@student.42.fr>
2020-02-19 15:09:57 +0100

    [FIX] Fixed existing events link print.

--- src/print_link_target_functions.c
+++ src/print_link_target_functions.c
@@ -6,7 +6,7 @@
 /*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 12:04:01 by lnicosia          #+#    #+#             */
-/*   Updated: 2020/02/14 19:40:25 by lnicosia         ###   ########.fr       */
+/*   Updated: 2020/02/19 15:08:55 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ t_point pos, int size)
 		ended = "ended (start)";
 	ft_snprintf(env->snprintf, SNPRINTF_SIZE, "%s %d %s",
 	env->event_links_types[condition->target_type],
-	condition->target_index, ended);
+	condition->target_trigger.index, ended);
 	/*print_text(pos, new_printable_text(env->snprintf,
 	font, 0xFFFFFFFF, 0), env);*/
 	return (pos.y);
@@ -51,7 +51,7 @@ t_point pos, int size)
 		ended = "ended (start)";
 	ft_snprintf(env->snprintf, SNPRINTF_SIZE, "Sector %d %s %d %s",
 	condition->sector, env->event_links_types[condition->target_type],
-	condition->target_index, ended);
+	condition->target_trigger.index, ended);
 	/*print_text(pos, new_printable_text(env->snprintf,
 	font, 0xFFFFFFFF, 0), env);*/
 	return (pos.y);
@@ -73,7 +73,7 @@ t_point pos, int size)
 		ended = "ended (start)";
 	ft_snprintf(env->snprintf, SNPRINTF_SIZE, "Enemy %d %s %d %s",
 	condition->enemy, env->event_links_types[condition->target_type],
-	condition->target_index, condition->value, ended);
+	condition->target_trigger.index, condition->value, ended);
 	/*print_text(pos, new_printable_text(env->snprintf,
 	font, 0xFFFFFFFF, 0), env);*/
 	return (pos.y);
@@ -95,7 +95,7 @@ t_point pos, int size)
 		ended = "ended (start)";
 	ft_snprintf(env->snprintf, SNPRINTF_SIZE, "Sector %d wall %d sprite %d"
 	" %s %d %s", condition->sector, condition->wall, condition->sprite,
-	env->event_links_types[condition->target_type], condition->target_index,
+	env->event_links_types[condition->target_type], condition->target_trigger.index,
 	ended);
 	/*print_text(pos, new_printable_text(env->snprintf,
 	font, 0xFFFFFFFF, 0), env);*/
