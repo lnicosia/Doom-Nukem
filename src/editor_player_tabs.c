9e2a85ffad555e6717775b4d7cc740f5e066c0a0
Simon <simonpatry12@gmail.com>
2020-02-25 14:34:42 +0100

    corrected a part of player selection in 2D

--- src/editor_player_tabs.c
+++ src/editor_player_tabs.c
@@ -6,7 +6,7 @@
 /*   By: sipatry <sipatry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 12:23:23 by sipatry           #+#    #+#             */
-/*   Updated: 2020/02/13 11:19:40 by lnicosia         ###   ########.fr       */
+/*   Updated: 2020/02/25 14:34:19 by sipatry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,16 +59,18 @@ int		print_player_general_tab(t_env *env)
 	print_text(new_point(520, 60), new_printable_text("Pos: ",
 	env->sdl.fonts.lato20, 0x00000000, 30), env);
 	ft_snprintf(env->snprintf, SNPRINTF_SIZE, "%.*f",
-	ft_min(1, get_decimal_len(env->player.pos.x)), env->player.pos.x);
-	env->editor.hud.g_player.t_pos_x.target = &env->player.pos.x;
+	ft_min(1, get_decimal_len(env->player.starting_pos.x)), env->player.starting_pos.x);
+	env->editor.hud.g_player.t_pos_x.target = &env->player.starting_pos.x;
 	draw_button(env, env->editor.hud.g_player.pos_x, env->snprintf);
 	ft_snprintf(env->snprintf, SNPRINTF_SIZE, "%.*f",
-	ft_min(1, get_decimal_len(env->player.pos.y)), env->player.pos.y);
+	ft_min(1, get_decimal_len(env->player.starting_pos.y)),
+	env->player.starting_pos.y);
 	env->editor.hud.g_player.t_pos_y.target = &env->player.pos.y;
 	draw_button(env, env->editor.hud.g_player.pos_y, env->snprintf);
 	ft_snprintf(env->snprintf, SNPRINTF_SIZE, "%.*f",
-	ft_min(1, get_decimal_len(env->player.pos.z)), env->player.pos.z);
-	env->editor.hud.g_player.t_pos_z.target = &env->player.pos.z;
+	ft_min(1, get_decimal_len(env->player.starting_pos.z)),
+	env->player.starting_pos.z);
+	env->editor.hud.g_player.t_pos_z.target = &env->player.starting_pos.z;
 	draw_button(env, env->editor.hud.g_player.pos_z, env->snprintf);
 	print_text(new_point(560, 60), new_printable_text("Health: ",
 	env->sdl.fonts.lato20, 0x00000000, 30), env);
