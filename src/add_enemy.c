9dfddef3ff88405fdfec755416d961f255d6d9df
lnicosia <lnicosia@student.42.fr>
2020-06-03 15:04:52 +0200

    [FIX] Enemies can not be placed in the void anymore.

--- src/add_enemy.c
+++ src/add_enemy.c
@@ -31,21 +31,8 @@ void	set_enemy_height_on_floor(t_enemy *enemy)
 	}
 }
 
-int		add_enemy(t_env *env)
+int		add_enemy2(t_enemy enemy, t_env *env)
 {
-	t_enemy	enemy;
-
-	ft_bzero(&enemy, sizeof(t_enemy));
-	enemy.num = env->nb_enemies;
-	enemy.pos.x = (env->sdl.mx - env->editor.center.x) / env->editor.scale;
-	enemy.pos.y = (env->sdl.my - env->editor.center.y) / env->editor.scale;
-	enemy.sprite = env->editor.current_enemy;
-	enemy.angle = 0;
-	enemy.exists = 1;
-	enemy.sector = get_sector_no_z(env, new_v3(enemy.pos.x, enemy.pos.y, 0));
-	enemy.speed = 40;
-	enemy.map_hp = 40;
-	enemy.damage = 25;
 	set_enemy_height_on_floor(&enemy);
 	if (!(env->enemies = (t_enemy*)ft_realloc(env->enemies,
 		sizeof(t_enemy) * env->nb_enemies, sizeof(t_enemy)
@@ -58,4 +45,30 @@ int		add_enemy(t_env *env)
 	env->editor.add_enemy.state = UP;
 	init_enemies_data(env);
 	return (0);
+}
+
+int		add_enemy(t_env *env)
+{
+	t_enemy	enemy;
+
+	ft_bzero(&enemy, sizeof(t_enemy));
+	enemy.num = env->nb_enemies;
+	enemy.pos.x = (env->sdl.mx - env->editor.center.x) / env->editor.scale;
+	enemy.pos.y = (env->sdl.my - env->editor.center.y) / env->editor.scale;
+	enemy.sprite = env->editor.current_enemy;
+	enemy.angle = 0;
+	enemy.exists = 1;
+	if ((enemy.sector = get_sector_no_z(env,
+		new_v3(enemy.pos.x, enemy.pos.y, 0))) == -1)
+	{
+		env->editor.create_enemy = 0;
+		if (update_confirmation_box(&env->confirmation_box,
+			"Enemy must be placed in a sector", ERROR, env))
+			return (-1);
+		return (0);
+	}
+	enemy.speed = 40;
+	enemy.map_hp = 40;
+	enemy.damage = 25;
+	return (add_enemy2(enemy, env));
 }
